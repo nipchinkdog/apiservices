d34e74b04ba13e9e3775c766886fbaa44c9b4494
nipchinkdog <nipchinkdog@gmail.com>
2014-05-07 13:03:02 +0800

    added files

---
+++ urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'service.views.home', name='home'),
+    # url(r'^service/', include('service.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^api/', include('apps.service._api._d2._urls._heroes._main')),
+    #! doc
+    url(r'^doc/', include('apps.service._urls._doc._d2._main')),
+
+)
