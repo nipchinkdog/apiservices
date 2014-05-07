d968b64e11518bde53831df7ee9cffe05cb89521
nipchinkdog <nipchinkdog@gmail.com>
2014-05-07 16:09:46 +0800

    edit

--- apps/service/_modules/_doc/_d2/_Doc_Home.py
+++ apps/service/_modules/_doc/_d2/_Doc_Home.py
@@ -14,26 +14,15 @@ class DocHome(View):
 
     def get(self, request):
         
-        #! requests
-        api_result = requests.get(self._api_doc_findbyhero)
-        #! request response variables
-        api_response_url = self._api_doc_findbyhero
-        api_response_status = api_result.status_code
-        api_response_text = json.loads(api_result.text)
-        api_response_json = api_result.json()        
-       
         return render_to_response(self._template, 
-                                  {#! demo
+                                  {
+                                   #! demo
+                                   'api_root_url' :  ROOT_URL,
                                    'api_doc_url_all' :  self._api_doc_all,
                                    'api_doc_url_findbyhero' :  self._api_doc_findbyhero,
                                    'api_doc_url_findbyfaction' :  self._api_doc_findbyfaction,
                                    'api_doc_url_findbyclass' :  self._api_doc_findbyclass,
                                    'api_doc_url_findbyany' :  self._api_doc_findbyany,
-                                   
-                                   'api_root_url' :  ROOT_URL,
-                                   'api_response_url' :  api_response_url,
-                                   'api_response_status' :  api_response_status,
-                                   'api_response_json' :  api_response_json,
-                                   'api_response_result' :  api_response_text }, 
+                                   }, 
                                   context_instance=RequestContext(request))
           
