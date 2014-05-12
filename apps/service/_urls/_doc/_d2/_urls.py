7279a7ace6c7773d683a2438c4818c7606edb3d3
nipchinkdog <nipchinkdog@gmail.com>
2014-05-12 22:21:37 +0800

    added cool files

--- apps/service/_urls/_doc/_d2/_urls.py
+++ apps/service/_urls/_doc/_d2/_urls.py
@@ -5,11 +5,12 @@ from django.conf.urls import url
 #! View
 from apps.service._modules._doc._d2 import _Doc_Home
 from apps.service._modules._doc._d2 import _Doc_Css
+from apps.service._modules._doc._d2 import _Doc_Download
 
 urlpatterns = patterns('',
     url(r'^$', _Doc_Home.DocHome.as_view(), name='doc_d2_home_api'),
     url(r'^index/$', _Doc_Home.DocHome.as_view(), name='doc_d2_home_api'),
     url(r'^api/$', _Doc_Home.DocHome.as_view(), name='doc_d2_home_api'),
     url(r'^css/$', _Doc_Css.DocCss.as_view(), name='doc_d2_home_css'),
-    url(r'^css/download$', _Doc_Css.DocCss.as_view(), name='doc_d2_home_css'),
+    url(r'^dl/$', _Doc_Download.DocDownload.as_view(), name='doc_d2_download_css'),
 )
