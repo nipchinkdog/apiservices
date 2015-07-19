eb19017b41f0cc2dba7589bfdd8e59dba889846a
nipchinkdog <nipchinkdog@gmail>
2015-07-19 08:22:30 +0800

    updates

--- apps/service/_cv/_urls/_main.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns
-from django.conf.urls import include
-from django.conf.urls import url
-
-urlpatterns = patterns('',
-    url(r'', include('apps.service._cv._urls._portfolio')),
-)
